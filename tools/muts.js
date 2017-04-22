5c95fbaecbff5021f6758a3a62c1db92b1ee7c9a
Victor <theonlypwner@users.noreply.github.com>
2017-04-21 20:03:48 -0600

    Add modes to mutator tool

--- tools/muts.js
+++ tools/muts.js
@@ -1,27 +1,73 @@
 /* global $ */
 
-var m = 0
+var mode = 0
+var muts = 0
+
+var GSP_START = 15
+var GSP_NUM = 1
 var NUM_MUTS = 16
 var MUT_ALL = (1 << (NUM_MUTS + 1)) - 1
 
+var MODES = [
+  ['demo', 'gsp1'],
+  ['coopedit', 'gsp1'],
+  ['deathmatch', 'survivor'],
+  ['capture the flag', 'direct'],
+  ['secure the flag', 'direct'],
+  ['hunt the flag', 'strict'],
+  ['keep the flag', 'double'],
+  ['bomber', 'demolition'],
+  ['zombies', 'progressive'],
+  ['overload', 'quick']
+]
+
+function updateMode () {
+  $('#mode').val(mode)
+  $('.btn-acr-mode').removeClass('active')
+  $('#mode' + mode).addClass('active')
+  for (var i = 0; i < GSP_NUM; ++i) {
+    $('#mut' + (GSP_START + i)).text(MODES[mode][1 + i])
+  }
+}
+
 function updateMutators () {
-  $('#mut').val(m)
+  $('#mut').val(muts)
   for (var i = 0; i < NUM_MUTS; ++i) {
-    $('#mut' + i).toggleClass('active', !!(m & (1 << i)))
+    $('#mut' + i).toggleClass('active', !!(muts & (1 << i)))
   }
 }
 
 $(function () {
+  // modes
+  $('#mode').change(function () {
+    mode = $('#mode').val() % MODES.length
+    if (mode < 0) mode += MODES.length
+    updateMode()
+  })
+  for (var i = 0; i < MODES.length; ++i) {
+    (function (i) {
+      $('#mode' + i)
+        .text(MODES[i][0])
+        .click(function () {
+          mode = i
+          updateMode()
+        })
+    })(i)
+  }
+  // mutators
   $('#mut').change(function () {
-    m = $('#mut').val() & MUT_ALL
+    muts = $('#mut').val() & MUT_ALL
     updateMutators()
   })
-  for (var i = 0; i < NUM_MUTS; ++i) {
+  for (i = 0; i < NUM_MUTS; ++i) {
     (function ($b, flag) {
       $b.click(function () {
-        m ^= flag
+        muts ^= flag
         updateMutators()
       })
     })($('#mut' + i), (1 << i))
   }
+
+  // default mode
+  $('#mode2').click()
 })
